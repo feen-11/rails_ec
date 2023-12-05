74271a4b5c8bc866c630cf58401075a4d916be51
taichi <taichishiozawa@gmail.com>
2023-12-05 20:57:28 +0900

    マイグレーションファイルを修正

--- db/migrate/20231127130027_drop_tasks.rb
+++ db/migrate/20231127130027_drop_tasks.rb
@@ -1,6 +1,5 @@
-class DropTasks < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :tasks do
-    end
+class DropTasks < ActiveRecord::Migration[7.0.8]
+  def up
+    drop_table :tasks if ActiveRecord::Base.connection.table_exists? 'tasks'
   end
 end
