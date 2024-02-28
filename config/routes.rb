cb9331fd801db81c63e1adea613b503094622446
taichi <taichishiozawa@gmail.com>
2024-02-28 20:56:31 +0900

    add: 買い物カートのview

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :products
   end
   resources :products
+  resources :carts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
