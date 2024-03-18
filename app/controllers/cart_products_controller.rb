51c30460012dd5e02b51e327e5559f84f46b72c6
taichi <taichishiozawa@gmail.com>
2024-03-18 21:23:44 +0900

    update: カートのレイアウト調整

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -22,8 +22,8 @@ class CartProductsController < ApplicationController
         redirect_to root_path, notice: '数量を更新できませんでした。'
       end
     else
-      @cart_product = CartProduct.new(cart_id: @cart.id, product_id: cart_product_create_params[:product_id], quantity: quantity, price: price)
-      if @cart_product.save
+      cart_product = CartProduct.new(cart_id: @cart.id, product_id: cart_product_create_params[:product_id], quantity: quantity, price: price)
+      if cart_product.save
         redirect_to root_path, notice: '商品をカートに追加しました。'
       else
         redirect_to root_path, notice: '商品をカートに追加できませんでした。'
