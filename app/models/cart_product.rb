2cb0c13551138b329d6893f8470c9ef9d0d38952
taichi <taichishiozawa@gmail.com>
2024-03-18 20:43:07 +0900

    update: cart_productモデルにバリデーション

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,5 +1,5 @@
 class CartProduct < ApplicationRecord
   belongs_to :cart
   belongs_to :product
-  validates :price, :quantity, presence: true, numericality: { only_integer: true }
+  validates :price, :quantity, :product_id, :cart_id, presence: true, numericality: { only_integer: true }
 end
