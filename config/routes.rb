1a436b2aa1233a63e67cd9cd5d554cc7b464bdee
taichi <taichishiozawa@gmail.com>
2024-03-26 21:05:52 +0900

    add: Ordersコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'orders/index'
+  get 'orders/create'
   root 'products#index'
   namespace :admin do
     resources :products
