7639bc635a23c210999272c175f0d4407e9da408
taichi <taichishiozawa@gmail.com>
2024-03-26 21:00:21 +0900

    add: db:migrateを実行

--- db/migrate/20240326114825_create_orders.rb
+++ db/migrate/20240326114825_create_orders.rb
@@ -1,8 +1,18 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.integer :postal_code, null: false
+      t.string :country, null: false
+      t.string :prefecture, null: false
+      t.string :address_1, null: false
+      t.string :address_2, null: false
+      t.string :credit_number, null: false
+      t.string :credit_name, null: false
+      t.string :credit_cvv, null: false
+      t.string :credit_expiration, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_29_070655) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_26_114825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,23 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_29_070655) do
   end
 
   create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.integer "postal_code", null: false
+    t.string "country", null: false
+    t.string "prefecture", null: false
+    t.string "address_1", null: false
+    t.string "address_2", null: false
+    t.string "credit_number", null: false
+    t.string "credit_name", null: false
+    t.string "credit_cvv", null: false
+    t.string "credit_expiration", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
