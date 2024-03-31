95d58c369964c95be110a3f709de07f16d16624a
taichi <taichishiozawa@gmail.com>
2024-03-31 20:23:29 +0900

    update: メールに商品ごとの名前、価格、合計金額を追加

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,6 +1,6 @@
 class OrderMailer < ApplicationMailer
   def ordered_email(order)
     @order = order
-    mail(to: @order.email, subject: '注文確認メール')
+    mail(to: @order.email, subject: '【ご注文ありがとうございます】注文確定のお知らせ')
   end
 end
