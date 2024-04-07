93edc23617f37fc4c6af904da8f4010165b48a9d
taichi <taichishiozawa@gmail.com>
2024-04-07 15:58:02 +0900

    add: promotions#update

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,2 +1,4 @@
 class PromotionsController < ApplicationController
+  def update
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,6 @@ Rails.application.routes.draw do
     resources :orders, only: %i[create show]
   end
   resources :orders, only: %i[index]
+  resources :promotions, only: %i[update]
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
