e1425a599b471d160f6b20994df49c9b3f1bb540
taichi <taichishiozawa@gmail.com>
2024-04-09 20:23:27 +0900

    update: コードが有効かどうかチェックするプロセスを追加

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,8 +2,8 @@
 
 class PromotionsController < ApplicationController
   def update
-    check_code_used_or_unused and return
-    # check_used_promotion and return if @current_cart.promotion
+    redirect_if_cart_has_promotion and return if @current_cart.promotion
+    redirect_if_promotion_invalid and return
 
     promotion = Promotion.find_by(code: promotion_params[:code])
     if promotion.update(used: true, cart_id: @current_cart.id)
@@ -19,17 +19,20 @@ class PromotionsController < ApplicationController
     params.require(:promotion).permit(:code)
   end
 
-  def check_used_promotion
+  #  現在のカートでプロモーションコードが適用されているかチェック
+  def redirect_if_cart_has_promotion
     if @current_cart.promotion
       redirect_to cart_products_path, notice: 'プロモーションコードは既に適用されています。'
+    end
+  end
+
+  # 有効なプロモーションコードかチェック
+  def redirect_if_promotion_invalid
+    promotion = Promotion.find_by(code: promotion_params[:code])
+    if promotion.nil? || promotion.used
+      redirect_to cart_products_path, notice: 'このプロモーションコードは無効です。コードを確認して、もう一度お試しください。'
       return true
     end
     false
-  end
-
-  def check_code_used_or_unused
-    code = Promotion.find_by(code: promotion_params[:code])
-    redirect_to cart_products_path, notice: 'このプロモーションコードは無効です。' if code.used
-    return true
   end
 end
